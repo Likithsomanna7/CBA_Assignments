d85dab999b79641aa087ef90b47380f9fbe47891
likith somanna <likithsomanna7@gmail.com>
2025-08-03 11:03:25 +0530

    dataTypes,variables,operators,conditionalStatements

---
+++ JavaScript/dataTypes,Variables,Operators,conditional_Statements.js
@@ -0,0 +1,28 @@
+let name="Likith";
+var age=20;
+const PI=3.14;
+console.log(name);
+console.log(age);
+console.log(PI);
+
+//this is a single line comment
+
+
+
+/* this is
+a multiline commnet*/
+
+console.log(age+PI);
+console.log(age-PI);
+console.log(age/PI);
+console.log(age*PI);
+console.log(2%3);
+console.log(2**3);
+
+
+
+
+let a=5;
+let b=10;
+if(a===b) console.log("True");
+else console.log("False");
