bc46dfb6b6f67ee3449dfc89800979122d2c3054
likith somanna <likithsomanna7@gmail.com>
2025-08-03 11:16:52 +0530

    dataTypes,variables,operators,conditionalStatements

--- JavaScript/file1.js
+++ JavaScript/file1.js
@@ -29,3 +29,32 @@ else console.log("False");
 if(a&&b)console.log("True");
 if(a||b)console.log("Flase");
 if(!a) console.log("false")
+
+
+
+const ag=18;
+if(ag>18)
+{
+    console.log("Eligible");
+}
+else if(ag<18)
+{
+    console.log("Not Eligible");
+}
+else
+{
+    console.log("Eligible");
+}
+
+switch(ag)
+{
+    case 18: console.log("Eligible");
+            break;
+    case 16:console.log("Not Eligible");
+            break;
+    case 21:console.log("Eligible");
+            break;
+    default:console.log("Not Eligible");
+}
+
+(ag>=18)?console.log("Eligible"):console.log("Not Eligible");
