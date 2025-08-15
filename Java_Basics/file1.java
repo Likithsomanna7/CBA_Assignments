03d2d495e211a4cbdab8d9a3c3c7d8e279ef957f
likith somanna <likithsomanna7@gmail.com>
2025-08-15 12:30:05 +0530

    datatypes, type conversion ,operators

--- Java_Basics/file1.java
+++ Java_Basics/file1.java
@@ -3,6 +3,44 @@ class file1
 {
     public static void main(String args[])
     {
-        System.out.println("Java");
+       // Primitive datatypes
+       byte a=127;  //1  bytes
+       short b=96;  //2 bytes
+       int c=1000;  //4 bytes
+       long d=19383;  //8 bytes
+       float e=12.5f;   //4 bytes
+       double f=134;    //8 bytes
+        System.out.println(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e+"\n"+f);
+
+    
+        //type conversion  lower to higher is possible but higher to lower not possible 
+
+        b=a; //possible
+        a=(byte)b;//stores the mod value
+
+
+        //  operators Arithmetic
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        //  Relational Operators
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+
+        //logical operator
+
+        System.out.println(1==1 && 1==1);
+        System.out.println(2==2 || 3==1 );
+        System.out.println(!(2==2 || 3==1) );
+        System.out.println(2==2 ^ 3==1 );
+
+
+       
     }
 }
